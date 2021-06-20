8fba28e1deccf1919b0899b46b3d453b8450cce3
katskurka <kati.skurka@gmail.com>
2021-06-20 13:45:25 -0400

    added js, learned about arrow functions!

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-'use strict';
+const calculatorDisplay = document.querySelector ('h2');
+const inputButtons = document.querySelectorAll('button');
+const clearButton = document.getElementById('clear-button');
 
-function sum() {
-
-}
-
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-funtion divide() {
+function sendNumberValue(number) {
   
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
+
+function placeDecimal() {
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+inputButtons.forEach((inputButton) => {
+  if (inputButton.classList.length === 0) {
+    inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
+  } else if (inputButton.classList.contains('operator')) {
+    inputButton.addEventListener('click', () => sendNumberValue(inputButton.value));
+  } else if (inputButton.classList.contains('decimal')) {
+    inputButton.addEventListener('click', () => placeDecimal());
+  }
+});
+
+function clearDisplay() {
+  calculatorDisplay.textContent = '0';
+}
+
+clearButton.addEventListener('click', clearDisplay);
